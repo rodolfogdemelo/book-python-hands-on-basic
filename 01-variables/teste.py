c4d57374e2eb0efcd5111e4c8c2e547fdd5326dd
Rodolfo <rodolfogdemelo@gmail.com>
2024-11-13 22:49:48 -0300

    Novo deploy

--- 01-variables/teste.py
+++ 01-variables/teste.py
@@ -2,4 +2,14 @@ class Aluno():
     def __init__(self, nome, curso):
         self.nome = nome
         self.curso = curso
-    
+
+
+code = input('Type your code')
+
+match code:
+    case 'bills':
+        print('Bills code')
+    case 'sales':
+        print('Sales code')
+    case default:
+        print('Wrong selection')
